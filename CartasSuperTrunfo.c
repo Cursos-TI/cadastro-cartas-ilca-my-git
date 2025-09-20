8ca97515463cf5741cbc1fc5e539f65e29860bde
ilca-my-git <neriregina005@gmail.com>
2025-09-20 19:53:35 +0000

    correcao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,4 @@
 
-
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações
@@ -70,54 +69,20 @@ scanf("%f", &pib);
 printf("Digite o número de pontos turísticos:\n");
 scanf("%d", &pontos_turisticos);
 
-// Entrada de dados - carta 2
-
-printf("Carta 2 \n");
-
-printf(" Digite o Estado:\n");
-scanf("%c", &estado2);
-
-printf("Digite o codigo:\n");
-scanf("%s", &codigo2);
-
-printf("Digite a cidade:\n");
-scanf("%s", &cidade2);
-
-printf("Digite a população:\n");
-scanf("%lu", &populacao2);
-
-printf("Digite a área:\n");
-scanf("%f", &area2);
-
-printf("Digite o PIB:\n");
-scanf("%f", &pib2);
-
-printf("Digite o número de pontos turísticos:\n");
-scanf("%d", &pontos_turisticos2);
-
-// Calculo Densidade populacional - Cartas 1 e 2 
+// Calculo Densidade populacional - Cartas 1  
 
 //Carta 1 - densidade 
 densidade = (float)populacao/area; 
 
-//Carta 2 - densidade 
-densidade2 = (float)populacao2/area2; 
-
-// Calculo PIB per capita - Cartas 1 e 2
+// Calculo PIB per capita - Cartas 1 
 
 // Carta 1 - PIB per capita 
 pib_per_capita = pib/(float)populacao;
 
-// Carta 2 - PIB per capita 
-pib_per_capita2 = pib2/(float)populacao2;
-
-// Calculo Super Poder - cartas 1 e 2
+// Calculo Super Poder - cartas 1 
 
 // Carta 1 - super poder
 super_poder = (float)populacao + area + pib + (float)pontos_turisticos + pib_per_capita + (1/densidade);
-
-// Carta 2 - super poder
-super_poder2 = (float)populacao2 + area2 + pib2 + (float)pontos_turisticos2 + pib_per_capita2 + (1/densidade2);
 
 //Saída de dados - carta 1
 
@@ -142,6 +107,47 @@ printf("Densidade populacional: %.2f \n", densidade);
 printf("PIB per capita: %.2f \n", pib_per_capita);
 
 printf("Super poder: %.2f \n", super_poder);
+
+
+// Entrada de dados - carta 2
+
+printf("Carta 2 \n");
+
+printf("Digite o Estado:\n");
+scanf(" %c", &estado2);
+
+printf("Digite o codigo:\n");
+scanf(" %s", &codigo2);
+
+printf("Digite a cidade:\n");
+scanf(" %s", &cidade2);
+
+printf("Digite a população:\n");
+scanf(" %lu", &populacao2);
+
+printf("Digite a área:\n");
+scanf("  %f", &area2);
+
+printf("Digite o PIB:\n");
+scanf(" %f", &pib2);
+
+printf("Digite o número de pontos turísticos:\n");
+scanf(" %d", &pontos_turisticos2);
+
+// Calculo Densidade populacional - Cartas 2
+
+//Carta 2 - densidade 
+densidade2 = (float)populacao2/area2; 
+
+// Calculo PIB per capita - Cartas 2
+
+// Carta 2 - PIB per capita 
+pib_per_capita2 = pib2/(float)populacao2;
+
+// Calculo Super Poder - cartas 2
+
+// Carta 2 - super poder
+super_poder2 = (float)populacao2 + area2 + pib2 + (float)pontos_turisticos2 + pib_per_capita2 + (1/densidade2);
 
 // Saída de dados - carta 2 
 
@@ -187,46 +193,46 @@ comparacao_super_poder = super_poder > super_poder2;
 
 if(comparacao_populacao == 1){
     printf("População - Carta 1 venceu (%d) \n", comparacao_populacao);
-    } else if(comparacao_populacao == 0) {
-        printf("População - Carta 2 venceu (%d) \n", comparacao_populacao);
-        }
+} else if(comparacao_populacao == 0) {
+    printf("População - Carta 2 venceu (%d) \n", comparacao_populacao);
+}
 
-        if(comparacao_area == 1){
-            printf("Área - Carta 1 venceu (%d) \n", comparacao_area);
-            } else if (comparacao_area == 0){
-                printf("Área - Carta 2 venceu (%d) \n", comparacao_area);
-                }
+if(comparacao_area == 1){
+    printf("Área - Carta 1 venceu (%d) \n", comparacao_area);
+} else if (comparacao_area == 0){
+    printf("Área - Carta 2 venceu (%d) \n", comparacao_area);
+}
 
-                if (comparacao_pib == 1){
-                    printf("PIB - Carta 1 venceu (%d) \n", comparacao_pib);
-                    } else if (comparacao_pib == 0){
-                        printf("PIB - Carta 2 venceu (%d) \n", comparacao_pib);
-                        }
+if (comparacao_pib == 1){
+    printf("PIB - Carta 1 venceu (%d) \n", comparacao_pib);
+} else if (comparacao_pib == 0){
+    printf("PIB - Carta 2 venceu (%d) \n", comparacao_pib);
+}
 
-                        if (comparacao_pontos_turisticos == 1){
-                            printf("Quantidade de pontos turísticos - Carta 1 venceu (%d) \n", comparacao_pontos_turisticos);
-                            } else if (comparacao_pontos_turisticos == 0){
-                                printf("Quantidade de pontos turísticos - Carta 2 venceu (%d) \n", comparacao_pontos_turisticos);
-                                }
+if (comparacao_pontos_turisticos == 1){
+    printf("Quantidade de pontos turísticos - Carta 1 venceu (%d) \n", comparacao_pontos_turisticos);
+} else if (comparacao_pontos_turisticos == 0){
+    printf("Quantidade de pontos turísticos - Carta 2 venceu (%d) \n", comparacao_pontos_turisticos);
+}
 
-                                if(comparacao_densidade == 1){
-                                    printf("Densidade populacional - Carta 1 venceu (%d) \n", comparacao_densidade);
-                                    } else if(comparacao_densidade == 0){
-                                        printf("Densidade Populacional - Carta 2 venceu (%d) \n", comparacao_densidade);
-                                        }
+if(comparacao_densidade == 1){
+    printf("Densidade populacional - Carta 1 venceu (%d) \n", comparacao_densidade);
+} else if(comparacao_densidade == 0){
+    printf("Densidade Populacional - Carta 2 venceu (%d) \n", comparacao_densidade);
+}
 
-                                        if(comparacao_pib_per_capita == 1){
-                                            printf("PIB per Capita - Carta 1 venceu (%d) \n", comparacao_pib_per_capita);
-                                            } else if(comparacao_pib_per_capita == 0){
-                                                printf("PIB per Capita - Carta 2 venceu (%d) \n", comparacao_pib_per_capita);
-                                                }
+if(comparacao_pib_per_capita == 1){
+    printf("PIB per Capita - Carta 1 venceu (%d) \n", comparacao_pib_per_capita);
+} else if(comparacao_pib_per_capita == 0){
+    printf("PIB per Capita - Carta 2 venceu (%d) \n", comparacao_pib_per_capita);
+}
 
-                                                if (comparacao_super_poder == 1){
-                                                    printf("Super Poder - Carta 1 venceu (%d) \n", comparacao_super_poder);
-                                                    } else if (comparacao_super_poder == 0) {
-                                                        printf("Super poder - Carta 2 venceu (%d) \n", comparacao_super_poder);
-                                                        }
+if (comparacao_super_poder == 1){
+    printf("Super Poder - Carta 1 venceu (%d) \n", comparacao_super_poder);
+} else if (comparacao_super_poder == 0) {
+    printf("Super poder - Carta 2 venceu (%d) \n", comparacao_super_poder);
+}
 
-                                                        //Fim do programa
+//Fim do programa
 
-                                                        }
+}
